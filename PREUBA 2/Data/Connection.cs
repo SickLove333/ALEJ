fa998d68145877bd134b4767b4182980bf08ce4e
Jose Carlos <jc.rodriguez@tunmovil.com>
2024-05-15 20:51:31 -0600

    - Se agregó la dependencia de MySql.Data - Se agregó el directorio Data y la clase Connection para ejecutar la conexión a la db - Se agregó un ejemplo en el botón Archivo del Form1 que al hacer click ejecuta la conexión e inserta los datos de un nuevo paciente

---
+++ PREUBA 2/Data/Connection.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace PREUBA_2.Data;
+
+internal class Connection
+{
+    MySqlConnection conex = new MySqlConnection();
+
+    static string server = "35.167.145.215";
+    static string database = "db_h_san_bernabe";
+    static string user = "u_poo";
+    static string password = "qoY&-i9IZi";
+    
+    string connectionString = "server="+server+"; user="+user+"; password="+password+"; database="+database+";";
+
+    public MySqlConnection openConn()
+    {
+        try
+        {
+            conex.ConnectionString = connectionString;
+            conex.Open();
+            MessageBox.Show("Se conectó a la base de datos");
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show("No se conectó a la base de datos. Error: "+e.ToString());
+            //Console.WriteLine(e);
+            //throw;
+        }
+
+        return conex;
+    }
+
+    public void closeConn()
+    {
+        conex.Close();
+    }
+}
--- PREUBA 2/Form1.cs
+++ PREUBA 2/Form1.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using PREUBA_2.Data;
+
 namespace PREUBA_2
 {
     public partial class Form1 : Form
@@ -75,9 +78,47 @@ namespace PREUBA_2
 
         private void button7_Click(object sender, EventArgs e)
         {
-            Form4 form4 = new Form4();
-            form4.Show();
-            this.Hide();
+            //Form4 form4 = new Form4();
+            //form4.Show();
+            //this.Hide();
+
+            // Esto es para probar si la conexión a la db es exitosa
+            /*
+            Data.Connection objConnection = new Data.Connection();
+            objConnection.openConn();
+            */
+            
+            // Ejemplo para guardar datos de un paciente
+
+            int idEmpleado = 1;
+            string nombre = "Juán";
+            string apellidoPaterno = "Pérez";
+            string apellidoMaterno = "López";
+            int edad = 24;
+            int sexo = 1; // (1 = Masculino, 2 = Femenino)
+            string email = "jperez@email.com";
+            string password = "jperez123";
+
+            try
+            {
+                Connection objConnection = new Connection();
+                String query = $"CALL sp_put_paciente({idEmpleado}, '{nombre}', '{apellidoPaterno}', '{apellidoMaterno}', '{edad}', '{sexo}', '{email}', '{password}');";
+                MySqlCommand command = new MySqlCommand(query, objConnection.openConn());
+                MySqlDataReader reader = command.ExecuteReader();
+                MessageBox.Show("Se guardó el paciente");
+                while (reader.Read())
+                {
+                    
+                }
+                objConnection.closeConn();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ocurrió un error: "+exception.ToString());
+            }
+
+
+
         }
     }
 }
